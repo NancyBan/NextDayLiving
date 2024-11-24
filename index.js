e2074038bfbc7c04b2ad8d31eed88031972f04e5
NancyBan <nancyzendrero@gmail.com>
2024-11-23 22:20:19 -0600

    correccion template city 1

--- index.js
+++
@@ -1,8 +0,0 @@
-let menu = document.querySelector('.bx-menu');
-let subMenu = document.querySelector('.submenu');
-
-document.addEventListener('DOMContentLoaded', function () {
-    menu.addEventListener('click', function (e) {
-        subMenu.classList.toggle("active");
-    });
-})
