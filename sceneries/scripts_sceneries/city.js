78cad1edc72fbba3cac95483e034610bd2e5180f
NancyBan <nancyzendrero@gmail.com>
2024-11-24 20:03:19 -0600

    desiciones si o no

--- sceneries/scripts_sceneries/city.js
+++ sceneries/scripts_sceneries/city.js
@@ -2,6 +2,22 @@ const trashBag = document.getElementById('trashBag');
 const boxQuestion = document.querySelector('.decisionTrashBag');
 
 trashBag.addEventListener('click', function(){
-    console.log("le picaste :D");
     boxQuestion.classList.toggle("active");
 });
+
+const yes = document.getElementById('trashBag'); //Variable para en caso de que el usuario pulse si
+
+trashBag.addEventListener('click', function(){
+    console.log("Si le picaste we");
+    
+});
+
+const no = document.getElementById('trashBag'); //Variable para en caso de que el usuario pulse no
+
+trashBag.addEventListener('click', function(){
+    console.log("No le picaste we");
+    
+});
+
+
+/*console.log("le picaste :D");*/
