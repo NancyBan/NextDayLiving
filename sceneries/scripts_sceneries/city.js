7e378b0db80847921485d210c0824dbc8dd4504b
NancyBan <nancyzendrero@gmail.com>
2024-11-24 19:04:58 -0600

    js del letrero 1

--- sceneries/scripts_sceneries/city.js
+++ sceneries/scripts_sceneries/city.js
@@ -2,5 +2,6 @@ const trashBag = document.getElementById('trashBag');
 const boxQuestion = document.getElementsByClassName('decisionTrashBag');
 
 trashBag.addEventListener('click', function(){
+    console.log("le picaste :D");
     boxQuestion.classList.toggle("active");
 });
