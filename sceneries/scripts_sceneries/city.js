3ffbc6bfabd3a6580dea23bea7a25e96cfa351ed
NancyBan <nancyzendrero@gmail.com>
2024-11-24 20:12:15 -0600

    desiciones si o no ahora si la buena x2

--- sceneries/scripts_sceneries/city.js
+++ sceneries/scripts_sceneries/city.js
@@ -7,14 +7,14 @@ trashBag.addEventListener('click', function(){
 
 const yes = document.getElementById('throwTrash'); //Variable para en caso de que el usuario pulse si
 
-trashBag.addEventListener('click', function(){
+question.addEventListener('click', function(){
     console.log("Si le picaste we");
     
 });
 
 const no = document.getElementById('noThrowTrash'); //Variable para en caso de que el usuario pulse no
 
-trashBag.addEventListener('click', function(){
+question.addEventListener('click', function(){
     console.log("No le picaste we");
 
 });
