e6778c6b5fd94aa192d6d1adb2f434975a1f4834
NancyBan <nancyzendrero@gmail.com>
2024-11-24 23:10:48 -0600

    gift basura 5.1

--- sceneries/scripts_sceneries/city.js
+++ sceneries/scripts_sceneries/city.js
@@ -7,6 +7,12 @@ yes.addEventListener('click', function(){
     giftTrash.innerHTML = '<img src="imgs_sceneries/basurita.gif">';
     trashBag.style.display = "none";
     giftTrash.setAttribute('class', 'gift');
+
+    setTimeout(()=> {
+        giftTrash.remove();
+        trashBag.style.display = "block"
+        giftTrash.setAttribute('', '');
+    }, 4660);
 });
 
 const no = document.getElementById('noThrowTrash'); //Variable para en caso de que el usuario pulse no
