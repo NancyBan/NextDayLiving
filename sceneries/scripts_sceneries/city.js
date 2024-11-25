2ba68400398228dd1c0c9b1227d12cf463ac483c
NancyBan <nancyzendrero@gmail.com>
2024-11-25 02:23:00 -0600

    basura ora si 1

--- sceneries/scripts_sceneries/city.js
+++ sceneries/scripts_sceneries/city.js
@@ -1,12 +1,13 @@
 const giftTrash = document.getElementById('giftTrash');
 
 const yes = document.getElementById('throwTrash'); //Variable para en caso de que el usuario pulse si
-/*
+
 yes.addEventListener('click', function(){
-    boxQuestion.classList.remove("active");
     giftTrash.innerHTML = '<img src="imgs_sceneries/basurita.gif">';
     trashBag.style.display = "none";
     giftTrash.setAttribute('class', 'gift');
+    
+    boxQuestion.classList.remove("active");
     const imgTrash = document.querySelector('#giftTrash img');
     
     setTimeout(()=> {
@@ -15,30 +16,7 @@ yes.addEventListener('click', function(){
         }
         giftTrash.setAttribute('class', '');
     }, 4000);
-});*/
-
-
-const count = 0;
-yes.addEventListener('click', function(){
-    boxQuestion.classList.remove("active");
-    giftTrash.innerHTML = '<img src="imgs_sceneries/basurita.gif">';
-    giftTrash.setAttribute('class', 'gift');    
-    trashBag.style.display = "none";
-
-    const imgTrash = document.querySelector('#giftTrash img');
-      count ++;
-    console.log(count);
-    setTimeout(() => {
-        if(imgTrash){ 
-            imgTrash.remove();
-        }
-        giftTrash.setAttribute('class', '');
-        trashBag.style.animation = "none";
-        trashBag.style.display = "block";
-    }, 4000);
-
 });
-
 
 const no = document.getElementById('noThrowTrash'); //Variable para en caso de que el usuario pulse no
 
