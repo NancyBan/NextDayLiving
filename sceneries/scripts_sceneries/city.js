91ee1e033474a3700134322e9d69b24eeddb2bc3
NancyBan <nancyzendrero@gmail.com>
2024-11-24 22:42:45 -0600

    gift basura 3

--- sceneries/scripts_sceneries/city.js
+++ sceneries/scripts_sceneries/city.js
@@ -1,8 +1,13 @@
+const trashBag = document.getElementById('trashBag');
+const giftTrash = document.getElementById('giftTrash');
+
 const yes = document.getElementById('throwTrash'); //Variable para en caso de que el usuario pulse si
 
 yes.addEventListener('click', function(){
     console.log("Si le picaste we");
-    
+    giftTrash.innerHTML = '<img src="imgs_sceneries/basurita.gif">';
+    trashBag.style.display = "none";
+    giftTrash.setAttribute('class', 'gift');
 });
 
 const no = document.getElementById('noThrowTrash'); //Variable para en caso de que el usuario pulse no
