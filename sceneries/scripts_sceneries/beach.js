6ab33e36dd83b30fce05331ef75b373378694b9b
NancyBan <nancyzendrero@gmail.com>
2024-11-27 00:27:31 -0600

    beach select y letrero 1

---
+++ sceneries/scripts_sceneries/beach.js
@@ -0,0 +1,7 @@
+const trash = document.getElementsByClassName('decisionTrashBag');
+const question = document.getElementsByClassName('question');
+
+trash.addEventListener('click', function(){
+    question.innerText = "Al mar JAJJAJ";
+});
+
--- sceneries/scripts_sceneries/city.js
+++ sceneries/scripts_sceneries/city.js
@@ -1,69 +1,6 @@
 const gifTrash = document.getElementById('gifTrash');
 
-const yes = document.getElementById('throwTrash'); //Variable para en caso de que el usuario pulse si
-/*
-este da una vuelta y el resto bien
-
-yes.addEventListener('click', function(){
-    trashBag.style.animation = 'none';
-    trashBag.offsetHeight;
-    trashBag.style.animation = "throw 5s linear";
-    
-    boxQuestion.classList.remove("active");
-    giftTrash.innerHTML = '<img src="imgs_sceneries/basurita.gif">';
-    giftTrash.setAttribute('class', 'gift');    
-    trashBag.style.display = "none";
-
-    const imgTrash = document.querySelector('#giftTrash img');
-    
-    setTimeout(() => {
-        if(imgTrash){ 
-            imgTrash.remove();
-        }
-        giftTrash.setAttribute('class', '');
-        trashBag.style.display = "block";
-    }, 4000);
-});
-
-
-yes.addEventListener('click', function(){
-   let clickCount = 0; // Contador de clics
-
-yes.addEventListener('click', function () {
-    clickCount++; // Incrementar el contador
-    console.log(`Se hizo clic ${clickCount} veces`);
-
-    if (clickCount === 1) {
-        // Acción para el primer clic
-        trashBag.style.animation = 'none';
-        trashBag.offsetHeight; // Forzar un reflujo para reiniciar la animación
-        trashBag.style.animation = "throw 5s linear";
-
-        boxQuestion.classList.remove("active");
-        giftTrash.innerHTML = '<img src="imgs_sceneries/basurita.gif">';
-        giftTrash.setAttribute('class', 'gift');
-        trashBag.style.display = "none";
-
-        const imgTrash = document.querySelector('#giftTrash img');
-
-        setTimeout(() => {
-            if (imgTrash) {
-                imgTrash.remove();
-            }
-            giftTrash.setAttribute('class', '');
-            trashBag.style.display = "block";
-        }, 4000);
-
-    } else if (clickCount === 2) {
-        // Acción para el segundo clic
-        console.log("Acción específica para el segundo clic.");
-        trashBag.style.display = "block";
-
-        // Reiniciar el contador si quieres que vuelva a empezar
-        clickCount = 0;
-    }
-});
-});*/
+const yes = document.getElementById('throwTrash');
 
 yes.addEventListener('click', function(){
 
@@ -74,11 +11,9 @@ yes.addEventListener('click', function(){
     boxQuestion.classList.remove("active");
     trashBag.style.display = "none";
     
-    // Mostramos el gif con la animación
     gifTrash.innerHTML = '<img src="imgs_sceneries/basurita.gif">';
     gifTrash.setAttribute('class', 'gif');
     
-    // Esperamos a que termine la animación
     const imgTrash = document.querySelector('#gifTrash img');
     
     setTimeout(() => {
