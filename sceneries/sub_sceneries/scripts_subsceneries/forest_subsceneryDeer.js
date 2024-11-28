0dcb8c3ae4ced20bc1e47fdd36307b9d26c7c622
NancyBan <nancyzendrero@gmail.com>
2024-11-28 03:15:56 -0600

    venao viajero 3

--- sceneries/sub_sceneries/scripts_subsceneries/forest_subsceneryDeer.js
+++ sceneries/sub_sceneries/scripts_subsceneries/forest_subsceneryDeer.js
@@ -1,6 +1,8 @@
 const sign = document.querySelector('.sign');
-const video = document.getElementById('video');
+const contentVideo = document.getElementById('video');
+const  video = document.querySelector('#video video');
 
 sign.addEventListener('click', function() {
-    video.style.display = "block";
+    video.setAttribute('src', 'imgs_sub_sceneries/Venado Viajero.mp4');
+    contentVideo.style.display = "block";
 });
