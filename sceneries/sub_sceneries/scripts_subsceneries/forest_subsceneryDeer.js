42c76cce21fa787a5844bb2ca7180ef5fc55efdb
NancyBan <nancyzendrero@gmail.com>
2024-11-28 03:32:23 -0600

    Escenarios playa y sus docs.

--- sceneries/sub_sceneries/scripts_subsceneries/forest_subsceneryDeer.js
+++ sceneries/sub_sceneries/scripts_subsceneries/forest_subsceneryDeer.js
@@ -5,4 +5,9 @@ const  video = document.querySelector('#video video');
 sign.addEventListener('click', function() {
     video.setAttribute('src', 'imgs_sub_sceneries/Venado Viajero.mp4');
     contentVideo.style.display = "block";
+
+    video.addEventListener('ended', function(){
+        contentVideo.style.display = "none";
+    });
+
 });
