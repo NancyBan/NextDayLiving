ca4d5b5a5d1718bedc6e587d5e36c3ab15b19eb4
NancyBan <nancyzendrero@gmail.com>
2024-12-01 15:43:30 -0600

    colocar segunda tortuga correccion tiempo

--- sceneries/sub_sceneries/scripts_subsceneries/beach_subsceneryBag.js
+++ sceneries/sub_sceneries/scripts_subsceneries/beach_subsceneryBag.js
@@ -4,8 +4,8 @@ const background = document.querySelector('#background img');
 setTimeout(()=> {
     sign2.style.visibility = "visible";
     sign2.style.opacity = "1";
-},2000);
+}, 2000);
 
 setTimeout(()=> {
     background.setAttribute('src', 'imgs_sub_sceneries/beach_backgroundTurtle2.jpg')
-}, 2000);
+}, 4000);
