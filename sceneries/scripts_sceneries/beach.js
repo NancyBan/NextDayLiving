c10e8fd1faa9b048d1c7f3c317c41a36ebab3386
NancyBan <nancyzendrero@gmail.com>
2024-12-01 22:29:58 -0600

    acomodo animacion bolsa

--- sceneries/scripts_sceneries/beach.js
+++ sceneries/scripts_sceneries/beach.js
@@ -1,11 +1,19 @@
 const trash = document.querySelector('.trash');
 const question = document.querySelector('.trash .question');
 
+const bag = document.getElementById('trashBag');
+
 trash.addEventListener('click', function(){
     question.innerText = "Al mar JAJJAJ";
+    
     setTimeout(() => {
-        window.location.href = 'sub_sceneries/beach_subsceneryBag.html';
-    }, 1000);
+        bag.style.animation = "throw 1s ease-in-out forwards";
+
+        setTimeout(() => {
+            window.location.href = 'sub_sceneries/beach_subsceneryBag.html';
+        }, 1200);
+
+    }, 200);
 });
 
 const trashBag = document.getElementById('trashBag');
@@ -14,7 +22,6 @@ trashBag.addEventListener('click', function(){
     trash.classList.toggle("active");
 });
 
-//const sea = document.querySelector('.sea');
 const physical = document.getElementById('physical');
 const chemical = document.getElementById('chemical');
 
