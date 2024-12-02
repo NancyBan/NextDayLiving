9a8179fe3a9c6d30ec27c0e4f55fbd3ebad54735
NancyBan <nancyzendrero@gmail.com>
2024-12-01 23:10:26 -0600

    imagenes incendio forestal

---
+++ sceneries/scripts_sceneries/forest.js
@@ -0,0 +1,17 @@
+const optionOff = document.getElementById('optionYes');
+const optionGo = document.getElementById('optionNo');
+const img = document.querySelector('.img_forestFire');
+
+optionGo.addEventListener(()=> {
+    img.style.visibility = "visible";
+    img.style.opacity = "1";
+    
+    setTimeout(() => {
+        img.src = 'img_sceneries/forest_backgroundFireForest2';
+
+        setTimeout(() => {
+            img.src = 'img_sceneries/forest_backgroundFireForest3';
+        },3000);
+    },3000);
+});
+
