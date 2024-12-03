38ab5bc6ecfedaa6de068f83a66f9121cb734ba3
NancyBan <nancyzendrero@gmail.com>
2024-12-02 23:25:57 -0600

    Full Oso

--- sceneries/sub_sceneries/scripts_subsceneries/forest_subsceneryBoy.js
+++ sceneries/sub_sceneries/scripts_subsceneries/forest_subsceneryBoy.js
@@ -1,5 +1,11 @@
+const textBoy = document.querySelector('.sign1');
+const frog = document.querySelector('.img2');
 const textFrog = document.querySelector('.sign2');
 
 setTimeout(()=> {
-
-}, 1000 );
+    frog.style.animation = "jump 1s linear forwards";
+    textFrog.style.visibility ="visible";
+    textFrog.style.opacity ="1";
+    textBoy.style.visibility = "hidden";
+    textBoy.style.opacity = "0";
+}, 1000);
