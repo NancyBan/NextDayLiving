4a01ed8dff0ffc1666e3cee3a982c36bb0bf8796
NancyBan <nancyzendrero@gmail.com>
2024-12-02 23:28:46 -0600

    ranita acomodo animacion js

--- sceneries/sub_sceneries/scripts_subsceneries/forest_subsceneryBoy.js
+++ sceneries/sub_sceneries/scripts_subsceneries/forest_subsceneryBoy.js
@@ -3,9 +3,9 @@ const frog = document.querySelector('.img2');
 const textFrog = document.querySelector('.sign2');
 
 setTimeout(()=> {
-    frog.style.animation = "jump 1s linear forwards";
     textFrog.style.visibility ="visible";
     textFrog.style.opacity ="1";
+    frog.style.animation = "jump 1s linear forwards";
     textBoy.style.visibility = "hidden";
     textBoy.style.opacity = "0";
 }, 1000);
