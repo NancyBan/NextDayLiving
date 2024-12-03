9e33683b38f6366f1c457443415c5402ce8fec7a
NancyBan <nancyzendrero@gmail.com>
2024-12-03 01:46:51 -0600

    Texto Rila 1.1

--- sceneries/sub_sceneries/scripts_subsceneries/beach_subsceneryBag.js
+++ sceneries/sub_sceneries/scripts_subsceneries/beach_subsceneryBag.js
@@ -26,7 +26,7 @@ setTimeout(()=> {
                 medusa.style.left = `${Math.random() * window.innerWidth}px`;
                 medusa.style.top = `${Math.random() * window.innerHeight}px`;
                 medusa.style.width = '10rem';
-                medusa.style.zIndex = '1000';
+                medusa.style.zIndex = '100';
                 document.body.appendChild(medusa);
                 count++;
         
