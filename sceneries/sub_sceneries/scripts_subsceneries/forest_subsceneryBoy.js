706d0cb0f601c83b65ec4770126e69d03a0ab147
NancyBan <nancyzendrero@gmail.com>
2024-12-03 05:05:10 -0600

    ranitas volando 3

--- sceneries/sub_sceneries/scripts_subsceneries/forest_subsceneryBoy.js
+++ sceneries/sub_sceneries/scripts_subsceneries/forest_subsceneryBoy.js
@@ -57,12 +57,11 @@ async function animateScene() {
         await sleep(330); // 33% de 3s
         frog3.style.animation = "fly3 1s linear forwards";
         
-        
         await sleep(660); // 66% de 3s
-        frog2.style.animation = "fly2 1s linear forwards";
+        frog1.style.animation = "fly1 1s linear forwards";
         
         await sleep(1000);
-        frog1.style.animation = "fly1 1s linear forwards";
+        frog2.style.animation = "fly2 1s linear forwards";
 
     } catch (error) {
         console.error('Error en la animación:', error);
