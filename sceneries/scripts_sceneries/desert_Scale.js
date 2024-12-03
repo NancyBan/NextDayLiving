a9919e2f093cb366cf7988ce32c089388298f84f
NancyBan <nancyzendrero@gmail.com>
2024-12-03 07:41:53 -0600

    Textos Desierto

--- sceneries/scripts_sceneries/desert_Scale.js
+++ sceneries/scripts_sceneries/desert_Scale.js
@@ -1,9 +1,12 @@
- const area_gota = document.getElementById('area1');
- const area_humo = document.getElementById('area2');
+const area_gota = document.getElementById('area1');
+const area_humo = document.getElementById('area2');
 const content_innundado = document.querySelector('.content_gif_inundado');
 const content_seco = document.querySelector('.content_gif_seco');
+const info1 = document.querySelector('.info1');
+const info2 = document.querySelector('.info2');
+const info3 = document.querySelector('.info3');
 
- area_gota.addEventListener('click', function(){
+area_gota.addEventListener('click', function(){
     setTimeout(()=>{
         content_innundado.style.visibility= "visible";
         content_innundado.style.opacity="1";
@@ -11,18 +14,19 @@ const content_seco = document.querySelector('.content_gif_seco');
         setTimeout(()=> {
             content_innundado.style.visibility= "hidden";
             content_innundado.style.opacity = "0";
-        }, 10300);
+            
+        }, 10400);
     }, 1000);
- });
- 
- area_humo.addEventListener('click', function(){
-   setTimeout(()=>{
-    content_seco.style.visibility= "visible";
-    content_seco.style.opacity= "1";
+});
 
-    setTimeout(()=> {
-        content_seco.style.visibility= "hidden";
-        content_seco.style.opacity= "0";
-    }, 10300);
-   }, 1000);
- });
+area_humo.addEventListener('click', function(){
+    setTimeout(()=>{
+        content_seco.style.visibility= "visible";
+        content_seco.style.opacity= "1";
+
+        setTimeout(()=> {
+            content_seco.style.visibility= "hidden";
+            content_seco.style.opacity= "0";
+        }, 10400);
+    }, 1000);
+});
